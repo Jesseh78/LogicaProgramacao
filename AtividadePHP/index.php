75731e8ee1e0414731d025184da41c44baf61269
José Ulisses Fernandes Junior <11252401443@alunos.umc.br>
2025-09-29 15:12:30 -0300

    estrutura basica da pagina para atividade de php

---
+++ AtividadePHP/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Atividade de PHP</title>
+</head>
+<body>
+	<h1>Exemplo de página única HTML + PHP</h1>
+	<?php
+		echo "Olá, mundo! Este é um bloco PHP integrado.";
+	?>
+</body>
+</html>
+<!DOCTYPE html>
+<html> 
