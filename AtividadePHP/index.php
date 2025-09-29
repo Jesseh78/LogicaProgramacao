187737291145d881e414f427304f5cef952d202e
José Ulisses Fernandes Junior <11252401443@alunos.umc.br>
2025-09-29 15:33:56 -0300

    estrutura da pagina

--- AtividadePHP/index.php
+++ AtividadePHP/index.php
@@ -1,17 +1,33 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Atividade de PHP</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Busca com Resultados</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-	<h1>Exemplo de página única HTML + PHP</h1>
-	<?php
-		echo "Olá, mundo! Este é um bloco PHP integrado.";
-	?>
+
+<div class="search-container">
+    <h2>Buscar</h2>
+    <form action="index.php" method="GET">
+        <input type="text" name="query" placeholder="Digite sua busca...">
+        <input type="submit" value="Buscar">
+    </form>
+</div>
+
+<div class="result-container">
+    <h3>Resultados:</h3>
+
+<?php // digite seu código PHP abaixo
+
+
+
+?>
+
+
+</div>
+
 </body>
 </html>
-<!DOCTYPE html>
-<html> 
+
